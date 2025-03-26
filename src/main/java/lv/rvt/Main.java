ea12f1bdcaf1e130488dd71f41d15669de4b3029
Kristiāna Belova <166118176+23DP1KBelo@users.noreply.github.com>
2025-03-26 12:48:40 +0000

    ascii art title

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -7,14 +7,16 @@ public class Main
 {
     public static void main( String[] args ) throws IOException
     {
-        // UserManager.users(); //Add users
+        //UserManager.users(); //Add users
         
-        UserManager.login(); //Login 
+        //UserManager.login(); //Login 
 
-        // ProductManager.productsByCategorie(); //get products in cart
+        //ProductManager.productsByCategorie(); //get products in cart
 
 
-        // Cart.showAllPurchases(); //see all Purchases
+        //Cart.showAllPurchases(); //see all Purchases
+
+        ConsoleManeger.title();
 
     }
 }
